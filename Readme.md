8fd89b17385fe42b478548b937c603ad99be90c8
Maxim Mironyuk <serieznyi@gmail.com>
2024-05-03 14:09:45 +0300

    Add validation and completion for info.json (#12)

--- Readme.md
+++ Readme.md
@@ -8,3 +8,8 @@
 [![Jetbrains plugin](https://img.shields.io/jetbrains/plugin/v/24333-factorio-api-completion.svg?style=flat-square)](https://plugins.jetbrains.com/plugin/24333-factorio-api-completion)
 
 This is a plugin add code completion for the Factorio Api.
+
+Plugin develops ideas from [knoxfighter/intellij-factorio-autocompletion](https://github.com/knoxfighter/intellij-factorio-autocompletion). 
+
+# Features
+- Validation and completion [info.json](https://wiki.factorio.com/Tutorial:Mod_structure#info.json) file
---
+++ src/test/kotlin/io/serieznyi/intellij/factorioapicompletition/intellij/FactorioModInfoSchemaProviderFactoryTest.kt
@@ -0,0 +1,33 @@
+package io.serieznyi.intellij.factorioapicompletition.intellij
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.jetbrains.jsonSchema.extension.SchemaType
+
+class FactorioModInfoSchemaProviderFactoryTest : BasePlatformTestCase() {
+
+    @org.junit.jupiter.api.BeforeEach
+    override fun setUp() {
+        super.setUp()
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    override fun tearDown() {
+        super.tearDown()
+    }
+
+    @org.junit.jupiter.api.Test
+    fun testProviderWork() {
+        val service = FactorioModInfoSchemaProviderFactory();
+        val project = getProject()
+        val providers = service.getProviders(project)
+
+        assertNotEmpty(providers)
+
+        val provider = providers.first()
+
+        assertNotNull(provider.schemaFile) // schema file defined
+        assertNotEmpty(provider.name.toList()) // name not empty
+        assertTrue(provider.schemaFile!!.exists()) // json file exists
+        assertEquals(SchemaType.embeddedSchema, provider.schemaType)
+    }
+}
