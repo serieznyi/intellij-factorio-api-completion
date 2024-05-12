07045fb1f4837a50e5c6a53e967c674737d6a969
Maxim Mironyuk <serieznyi@gmail.com>
2024-05-12 21:54:59 +0300

    Cache getting api data by HTTP request for tests
    [s] data source cache

--- src/main/kotlin/io/serieznyi/intellij/factorioapicompletion/core/factorio/api/parser/ApiParser.kt
+++ src/main/kotlin/io/serieznyi/intellij/factorioapicompletion/core/factorio/api/parser/ApiParser.kt
@@ -4,13 +4,14 @@ import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.parser.data
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.parser.data.PrototypeApiData
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.parser.data.RuntimeApiData
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.parser.util.GsonBuilderHolder
+import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.source.DataSourceFactory
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.version.ApiVersion
-import java.io.IOException
-import java.io.InputStream
 import java.io.InputStreamReader
-import java.net.URL
 
-class ApiParser(private val throwOnUnknownType: Boolean = false) {
+class ApiParser(
+    private val dataSourceFactory: DataSourceFactory,
+    private val throwOnUnknownType: Boolean = false
+) {
     fun parse(apiVersion: ApiVersion): ApiData {
         return ApiData(
             parsePrototypeApiData(apiVersion),
@@ -19,7 +20,7 @@ class ApiParser(private val throwOnUnknownType: Boolean = false) {
     }
 
     private fun parseRuntimeApi(apiVersion: ApiVersion): RuntimeApiData {
-        InputStreamReader(createJsonStream(RUNTIME_API_BASE_URL, apiVersion)).use { reader ->
+        InputStreamReader(dataSourceFactory.createJsonStreamForRuntime(apiVersion)).use { reader ->
             return GsonBuilderHolder
                 .gson(this.throwOnUnknownType)
                 .create()
@@ -28,23 +29,11 @@ class ApiParser(private val throwOnUnknownType: Boolean = false) {
     }
 
     private fun parsePrototypeApiData(apiVersion: ApiVersion): PrototypeApiData {
-        InputStreamReader(createJsonStream(PROTOTYPE_API_BASE_URL, apiVersion)).use { reader ->
+        InputStreamReader(dataSourceFactory.createJsonStreamForPrototype(apiVersion)).use { reader ->
             return GsonBuilderHolder
                 .gson(this.throwOnUnknownType)
                 .create()
                 .fromJson(reader, PrototypeApiData::class.java)
         }
-    }
-
-    @Throws(IOException::class)
-    private fun createJsonStream(template: String, version: ApiVersion): InputStream {
-        val url: String = String.format(template, version.version)
-
-        return URL(url).openStream()
-    }
-
-    companion object {
-        private const val PROTOTYPE_API_BASE_URL = "https://lua-api.factorio.com/%s/prototype-api.json"
-        private const val RUNTIME_API_BASE_URL = "https://lua-api.factorio.com/%s/runtime-api.json"
     }
 }
--- src/test/kotlin/io/serieznyi/intellij/factorioapicompletion/core/factorio/api/parser/ApiParserTest.kt
+++ src/test/kotlin/io/serieznyi/intellij/factorioapicompletion/core/factorio/api/parser/ApiParserTest.kt
@@ -1,19 +1,37 @@
 package io.serieznyi.intellij.factorioapicompletion.core.factorio.api.parser
 
+import com.intellij.openapi.util.io.findOrCreateDirectory
+import io.serieznyi.intellij.factorioapicompletion.core.cache.FileCache
+import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.source.FileCacheDataSourceFactory
+import io.serieznyi.intellij.factorioapicompletion.core.factorio.api.source.HttpDataSourceFactory
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.version.ApiVersion
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.version.ApiVersionResolverHolder
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.io.CleanupMode
+import org.junit.jupiter.api.io.TempDir
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.Duration
 import java.util.stream.Stream
 
 class ApiParserTest {
-    private val apiParser = ApiParser(throwOnUnknownType = true);
+    private lateinit var apiParser: ApiParser
 
     @BeforeEach
-    fun setUp() {
+    fun setUp(@TempDir(cleanup = CleanupMode.NEVER) tempDir: Path) {
+        val cacheDir = Paths.get(System.getenv("BUILD_DIR_PATH")).resolve(ApiParserTest::class.simpleName.toString())
+
+        apiParser = ApiParser(
+            FileCacheDataSourceFactory(
+                HttpDataSourceFactory(),
+                FileCache(cacheDir.findOrCreateDirectory(), Duration.ofDays(14))
+            ),
+            throwOnUnknownType = true
+        )
     }
 
     @AfterEach
