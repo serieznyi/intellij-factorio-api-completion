5a4ae2685d0d04fcd1a3cdf4ed0b76cc6c90d61d
Maxim Mironyuk <serieznyi@gmail.com>
2024-05-21 08:10:05 +0300

    Index generated api files

--- src/main/kotlin/io/serieznyi/intellij/factorioapicompletion/intellij/FactorioApiProvider.kt
+++ src/main/kotlin/io/serieznyi/intellij/factorioapicompletion/intellij/FactorioApiProvider.kt
@@ -1,67 +1,81 @@
 package io.serieznyi.intellij.factorioapicompletion.intellij
 
 import com.intellij.navigation.ItemPresentation
-import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ex.ProjectManagerEx
 import com.intellij.openapi.roots.AdditionalLibraryRootsProvider
 import com.intellij.openapi.roots.SyntheticLibrary
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx
 import com.intellij.openapi.util.EmptyRunnable
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.stubs.StubIndex
 import com.tang.intellij.lua.lang.LuaIcons
-import io.serieznyi.intellij.factorioapicompletion.core.factorio.version.ApiVersion
+import java.nio.file.Path
 import javax.swing.Icon
 
 class FactorioApiProvider: AdditionalLibraryRootsProvider() {
-
     override fun getAdditionalProjectLibraries(project: Project): Collection<SyntheticLibrary> {
-        return listOf()
+//        val apiPath = project.service<ApiService>().apiDir()
+        val libs = ArrayList<FactorioLibrary>()
+
+//        if (apiPath.exists()) {
+//            libs.add(createLibrary(apiPath, "Factorio API"))
+//        }
+
+        return libs
     }
 
     companion object {
-        fun reload() {
-            WriteAction.run<RuntimeException> {
-                val openProjects = ProjectManagerEx.getInstanceEx().openProjects
-                for (openProject in openProjects) {
-                    ProjectRootManagerEx
-                        .getInstanceEx(openProject)
-                        .makeRootsChange(EmptyRunnable.getInstance(), false, true)
-                }
+        fun reload(project: Project) {
+//            ApplicationManager.getApplication().invokeLater({
+//                StubIndex.getInstance().forceRebuild(Throwable("Factorio API changed."))
+//
+//                ProjectRootManagerEx.getInstanceEx(project).makeRootsChange(EmptyRunnable.getInstance(), false, true)
+//            }, project.disposed)
 
-                StubIndex.getInstance().forceRebuild(Throwable("Factorio API changed"))
-            }
+
+            // todo trigger update index event
+
+//            StubIndex.getInstance().forceRebuild(Throwable("Factorio API changed."))
+//
+//            WriteAction.run<RuntimeException> {
+//                ProjectRootManagerEx.getInstanceEx(project).makeRootsChange(EmptyRunnable.getInstance(), false, true)
+//            }
         }
     }
 
-    class FactorioLibrary : SyntheticLibrary(), ItemPresentation {
-        lateinit var root: VirtualFile
-        lateinit var apiVersion: ApiVersion
+    private fun createLibrary(libraryDir: Path, libraryName: String): FactorioLibrary {
+        val virtualLibraryDir = VfsUtil.findFileByIoFile(libraryDir.toFile(), true) ?: throw IllegalArgumentException("Wrong lib dir: $libraryDir")
 
-        fun FactorioLibrary(root: VirtualFile, apiVersion: ApiVersion) {
-            this.root = root
-            this.apiVersion = apiVersion
+//        virtualLibraryDir.children.forEach { it.putUserData(LuaFileUtil.PREDEFINED_KEY, true) }
+
+        return FactorioLibrary(virtualLibraryDir, libraryName)
+    }
+
+    private class FactorioLibrary(var root: VirtualFile, var libraryName: String) : SyntheticLibrary(), ItemPresentation {
+        override fun hashCode(): Int {
+            return root.hashCode()
         }
 
         override fun equals(other: Any?): Boolean {
             return (other is FactorioLibrary) && other.root == root
         }
 
-        override fun hashCode(): Int {
-            return root.hashCode()
+        override fun getSourceRoots(): Collection<VirtualFile> {
+            return listOf(root)
         }
 
-        override fun getPresentableText(): String {
-            return apiVersion.toString()
+        override fun getLocationString(): String {
+            return "Factorio library"
         }
 
         override fun getIcon(unused: Boolean): Icon? {
             return LuaIcons.FILE
         }
 
-        override fun getSourceRoots(): List<VirtualFile> {
-            return listOf(root)
+        override fun getPresentableText(): String {
+            return libraryName
         }
     }
 }
