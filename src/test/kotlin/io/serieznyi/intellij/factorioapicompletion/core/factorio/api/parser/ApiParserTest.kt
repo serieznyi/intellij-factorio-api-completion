a9e903d63f13c059931711f779251bd1f3742c53
Maxim Mironyuk <serieznyi@gmail.com>
2024-05-23 08:27:07 +0300

    update test

--- src/test/kotlin/io/serieznyi/intellij/factorioapicompletion/core/factorio/api/parser/ApiParserTest.kt
+++ src/test/kotlin/io/serieznyi/intellij/factorioapicompletion/core/factorio/api/parser/ApiParserTest.kt
@@ -3,6 +3,8 @@ package io.serieznyi.intellij.factorioapicompletion.core.factorio.api.parser
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.version.ApiVersion
 import io.serieznyi.intellij.factorioapicompletion.core.factorio.version.ApiVersionResolver
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.params.ParameterizedTest
@@ -23,9 +25,20 @@ class ApiParserTest {
     @ParameterizedTest
     @MethodSource("parseProvider")
     fun parse(apiVersion: ApiVersion) {
-        assertDoesNotThrow {
-            apiParser.parse(apiVersion)
-        }
+        val apiData = apiParser.parse(apiVersion)
+        val runtimeApiData = apiData.runtimeApiData
+        val prototypeApiData = apiData.prototypeApiData
+
+        assertFalse(runtimeApiData.events.isEmpty())
+        assertFalse(runtimeApiData.classes.isEmpty())
+        assertFalse(runtimeApiData.defines.isEmpty())
+        assertFalse(runtimeApiData.concepts.isEmpty())
+        assertFalse(runtimeApiData.builtinTypes.isEmpty())
+        assertFalse(runtimeApiData.globalFunctions.isEmpty())
+        assertFalse(runtimeApiData.globalObjects.isEmpty())
+
+        assertFalse(prototypeApiData.prototypes.isEmpty())
+        assertFalse(prototypeApiData.types.isEmpty())
     }
 
     companion object {
